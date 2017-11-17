7a52a48c423c17b9e54b2f0762e517e3d491e1ba
Afonso Trêpa Cunha <afonsotrepa@gmail.com>
2017-11-17 18:30:16 +0000

    Added the ability to edit the url of the page being viewed

--- app/src/main/java/com/gmail/afonsotrepa/pocketgopher/gopherclient/Activity/MenuActivity.java
+++ app/src/main/java/com/gmail/afonsotrepa/pocketgopher/gopherclient/Activity/MenuActivity.java
@@ -1,16 +1,21 @@
 package com.gmail.afonsotrepa.pocketgopher.gopherclient.Activity;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,6 +39,7 @@ public class MenuActivity extends AppCompatActivity
     String selector;
     String server;
     Integer port;
+    String url;
 
 
     @Override
@@ -64,8 +70,9 @@ public class MenuActivity extends AppCompatActivity
                 selector = p.selector;
                 server = p.server;
                 port = p.port;
+                url = p.url;
 
-                setTitle(p.url);
+                setTitle(url);
 
                 ///Network stuff
                 List<Page> lines;
@@ -154,6 +161,53 @@ public class MenuActivity extends AppCompatActivity
                 {
                     throw new RuntimeException(e);
                 }
+
+                return true;
+
+            case R.id.link:
+                //create the dialog to be shown when the button gets clicked
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setMessage("URL:");
+
+                //setup the EditText where the user will input url to the page
+                final EditText input = new EditText(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                );
+                input.setLayoutParams(layoutParams);
+                input.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+                input.setText(url);
+                alertDialog.setView(input);
+
+
+                alertDialog.setPositiveButton("Go",
+                        new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(final DialogInterface dialog, int which)
+                            {
+                                //setup the page
+                                Page page = Page.makePage(input.getText().toString());
+
+                                page.open(getApplicationContext());
+                            }
+                        }
+                );
+
+
+                alertDialog.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        }
+                );
+
+                alertDialog.show();
 
                 return true;
 
